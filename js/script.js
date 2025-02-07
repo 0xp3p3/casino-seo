8405d48c1b0fe7c78789549920c95a188710c970
0xPePe <tiandage719@outlook.com>
2025-02-07 06:04:37 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function openLink() {
+  window.open("https://trafflnc.com/login?subid1=777Bonus.bet ", "_blank");
+}
+
+function openNav() {
+  document.getElementById("sidepanel").style.width = "100vw";
+}
+
+function closeNav() {
+  document.getElementById("sidepanel").style.width = "0";
+}
